3b26097670f53a7dcd86dbaabbaabb7ff799ef85
Martin S. Boswell <mboswell@me.com>
2015-05-03 16:03:56 -0700

    Can create Script folders and files (empty) in heirarchy.

--- lib/fmpvc/FMPReport.rb
+++ lib/fmpvc/FMPReport.rb
@@ -5,24 +5,27 @@ module FMPVC
   
   class FMPReport
     
-    attr_reader :content, :type, :text_dir, :text_filename, :report_filepath
+    attr_reader :content, :type, :text_dir, :text_filename, :report_dirpath
     
     def initialize(report_filename, ddr)
-      report_filepath = "#{ddr.base_dir}/#{report_filename}"  # location of the fmpfilename.xml file
-      raise(RuntimeError, "Error: can't find the report file, #{report_filepath}") unless File.readable?(report_filepath)
+      report_dirpath = "#{ddr.base_dir}/#{report_filename}"  # location of the fmpfilename.xml file
+      raise(RuntimeError, "Error: can't find the report file, #{report_dirpath}") unless File.readable?(report_dirpath)
       
-      @content = IO.read(report_filepath, mode: 'rb:UTF-16:UTF-8') # transcode is specifically for a spec match
+      @content = IO.read(report_dirpath, mode: 'rb:UTF-16:UTF-8') # transcode is specifically for a spec content match
       @text_dir = "#{ddr.base_dir}../fmp_text"
       @text_filename = fs_sanitize(report_filename)
-      @report_filepath = "#{@text_dir}/#{@text_filename}"
+      @report_dirpath = "#{@text_dir}/#{@text_filename}"
+      @scripts_dirpath = @report_dirpath + "/Scripts"
       
       self.parse
+      self.write_dir
+      self.write_scripts
       
     end
 
     def parse
-      report = Nokogiri::XML(@content)
-      @type = report.xpath("//FMPReport").first["type"]
+      @report = Nokogiri::XML(@content)
+      @type = @report.xpath("//FMPReport").first["type"]
       
       # the report should be a "Report" type
       raise RuntimeError, "Incorrect file type: not an FMPReport Report file" unless @type == "Report"
@@ -33,11 +36,44 @@ module FMPVC
       text_string.gsub(%r{[.\/\\]}mx, '_') # just remove [ . / \ ] for now.
     end
     
+    # e.g. /FMPReport/File/ScriptCatalog , /FMPReport/File/ScriptCatalog/Group[1]/Group
+    # return: "/Actors/Actor Triggers"
+    def disk_path_from_base(object_base, object_xpath, path = '')
+      return "#{path}" if object_xpath == object_base
+      curent_node_filename = @report.xpath("#{object_xpath}").first['name']
+      parent_node_xpath = @report.xpath("#{object_xpath}/..").first.path
+      disk_path_from_base(object_base,  parent_node_xpath, "/#{curent_node_filename}" + "#{path}" )
+    end
+    
     def write_dir
       # raise(RuntimeError, "Error: there is no text output base dir (e.g. /fmp_text)") unless File.readable?(@text_dir)  # needed with _p?
-      FileUtils.mkdir_p(@report_filepath)
+      FileUtils.mkdir_p(@report_dirpath)
+    end
+    
+    def write_scripts(object_xpath = '/FMPReport/File/ScriptCatalog')
+      current_disk_folder = disk_path_from_base('/FMPReport/File/ScriptCatalog', object_xpath)
+      # puts "Current disk folder: #{current_disk_folder}.  Folders: #{@report.xpath("#{object_xpath}/*[name()='Group']").each {|f| print f['name']}}"
+      
+      script_groups = @report.xpath("#{object_xpath}/*[name()='Group']")
+      script_groups.each do |a_folder|
+        full_folder_path = @scripts_dirpath + "#{current_disk_folder}/#{a_folder['name']}"
+        # puts "Full folder path: #{full_folder_path}"
+        FileUtils.mkdir_p(full_folder_path)
+        write_scripts(a_folder.path)
+      end
+      
+      scripts = @report.xpath("#{object_xpath}/*[name()='Script']")
+      scripts.each do |a_script|
+        script_name = a_script["name"]
+        # puts "Script name: #{script_name}"
+        this_script_disk_path = @scripts_dirpath + "/#{current_disk_folder}"
+        FileUtils.mkdir_p(this_script_disk_path) unless File.readable?(this_script_disk_path)
+        File.open(this_script_disk_path + "/#{script_name}.txt", 'w') do |f| f.puts "a script" end
+      end
     end
     
   end
 
 end
+
+#  can FMP folders have scripts and folders with same name?  rename 2nd one with a DUP suffix
--- spec/fmpvc/fmpreport_spec.rb
+++ spec/fmpvc/fmpreport_spec.rb
@@ -3,13 +3,19 @@ include FMPVC
 
 describe 'FMPReport' do
   
-  let (:ddr)         { double("ddr", :base_dir => :'./spec/data/test_1/fmp_ddr/') }
+  # clear the fmp_text directory (to make tests accurate for current iteration)
+  
+    
+  let (:ddr1)        { double("ddr", :base_dir => :'./spec/data/test_1/fmp_ddr/') }
   let (:report_file) { 'Movies_fmp12.xml' }
-  let (:report1)     { FMPReport.new(report_file, ddr) }
+  let (:report1)     { FMPReport.new(report_file, ddr1) }
+  
+  let (:ddr2)        { double("ddr", :base_dir => :'./spec/data/test_2/fmp_ddr/') }
+  let (:report2)     { FMPReport.new(report_file, ddr2) }
   
   it "should create an fmreport from a file" do
     expect(report1).to be_instance_of(FMPReport)
-    expect { FMPReport.new('path.xyz', ddr) }.to raise_error(RuntimeError)
+    expect { FMPReport.new('path.xyz', ddr1) }.to raise_error(RuntimeError)
   end
   
   it "should read the file content" do
@@ -18,26 +24,34 @@ describe 'FMPReport' do
   
   it "should throw an error if the type isn't Report" do
     expect(report1.type).to eq("Report")
-    expect {FMPReport.new("Summary.xml", ddr)}.to raise_error(RuntimeError) # bad report type
+    expect {FMPReport.new("Summary.xml", ddr1)}.to raise_error(RuntimeError) # bad report type
   end
   
   
 
-  describe '#write_dir' do
-  
+  describe '#write_dir' do  
     it "should create a filename_fmp12 directory on disk" do
-      report1.write_dir
-      expect(File.readable?(report1.report_filepath)).to be true
-    end
-  
+      # report1.write_dir
+      expect(File.readable?(report1.report_dirpath)).to be true
+      expect(File.readable?(report2.report_dirpath)).to be true
+    end  
   end
 
   describe '#write_scripts' do
-  
-    it "should create a script file on disk"
-  
-  
+    it "should create a script file on disk" do
+      # report1.write_scripts
+      expect(Dir.glob(report1.report_dirpath + "/Scripts/*.txt").count).to eq(1)
+    end
+    it "should create script folders" do
+      # report2.write_scripts
+      expect(File.directory?(report2.report_dirpath + "/Scripts/Actors")).to be true
+    end
+    it "should create nested script folders"
   end
+
+  
+
+
 
   describe '#write_tables' do
   
